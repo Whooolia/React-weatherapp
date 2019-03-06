8c55e0a6e19e36dd8b6a1ba070edba20bdd1246d
[Whooolia] <[tpdms2779@gmail.com]>
2019-03-06 22:34:49 +0000

    Updates

---
+++ precache-manifest.2ad28e4a9c124ecd47a42a86e67c1be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e6b57cce8920de6f8826",
+    "url": "/static/js/main.e6b57cce.chunk.js"
+  },
+  {
+    "revision": "f045d49857aa7d731cff",
+    "url": "/static/js/2.f045d498.chunk.js"
+  },
+  {
+    "revision": "e6b57cce8920de6f8826",
+    "url": "/static/css/main.43fa3471.chunk.css"
+  },
+  {
+    "revision": "d417b2aa6ad37edcee1e11c28a210a6a",
+    "url": "/index.html"
+  }
+];
